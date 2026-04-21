75c77afbe1271cf4dc9b6e07a866330ca339875f
BenHoule <benjhoule@hotmail.com>
2026-04-21 15:10:41 -0400

    Enhance build script and documentation: update build.sh for improved PDF generation, modify references.bib to add and reorder entries, and enrich llvmanim.tex with new figures and architectural details. Refine design goals and discussion sections for clarity and completeness.

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-rm -rf _minted-proposal && pdflatex -shell-escape -interaction=nonstopmode proposal.tex
+rm -rf _minted-proposal && \
+  pdflatex -shell-escape -interaction=nonstopmode proposal.tex && \
+  bibtex proposal && \
+  pdflatex -shell-escape -interaction=nonstopmode proposal.tex && \
+  pdflatex -shell-escape -interaction=nonstopmode proposal.tex
